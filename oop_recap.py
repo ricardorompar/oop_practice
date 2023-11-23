bb91c9e3fe1aabc2d2ce78b5f1dc0848856b227e
ricardorompar <ricardorompar@hotmail.com>
2023-11-23 15:45:49 +0100

    created new class Counter and method increase()

--- oop_recap.py
+++ oop_recap.py
@@ -9,4 +9,13 @@ class Chair:
 
 chair1 = Chair("Black", 4)
 
-print()
+class Counter:
+    def __init__(self, number_of_people = 0):
+        self.number_of_people = number_of_people
+    
+    def increase(self):
+        self.number_of_people += 1
+
+counter1 = Counter()
+counter1.increase()
+print(counter1.number_of_people)
